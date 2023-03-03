e4987a3a7bd2b4773df9f00dcd51abf4ca056d02
mingyo0125 <wjdalsry125@gmail.com>
2023-03-03 12:44:05 +0900

    2023_03_03

---
+++ 2023_03_03/2023_03_03/2023_03_03.cpp
@@ -0,0 +1,13 @@
+﻿// 2023_03_03.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char arr[100];
+	cout << "what is your name?" << endl;
+	cin >> arr;
+	cout << "hi" << " " << arr;
+}
